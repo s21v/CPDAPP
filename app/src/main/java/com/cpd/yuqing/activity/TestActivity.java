ed30a131a762c85a0f9ea5da56ffd61b1a63a6dd
s21v <s21vwy@aliyun.com>
2018-04-11 16:55:58 +0800

    添加数字报矩形计算

--- app/src/main/java/com/cpd/yuqing/activity/TestActivity.java
+++ app/src/main/java/com/cpd/yuqing/activity/TestActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cpd.yuqing.util.SplitRect;
 
@@ -15,33 +16,178 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinkedList<Point> inputPoints = new LinkedList<>();
-        inputPoints.add(new Point(100, 100));      //1
-        inputPoints.add(new Point(150, 100));    //2
-        inputPoints.add(new Point(150, 50));   //3
-        inputPoints.add(new Point(200, 50));     //4
-        inputPoints.add(new Point(200, 100));   //5
-        inputPoints.add(new Point(250, 100));   //6
-        inputPoints.add(new Point(250, 50));    //7
-        inputPoints.add(new Point(300, 50));    //8
-        inputPoints.add(new Point(300, 100));   //9
-        inputPoints.add(new Point(350, 100));   //10
-        inputPoints.add(new Point(350, 150));   //11
-        inputPoints.add(new Point(300, 150));   //12
-        inputPoints.add(new Point(300, 200));   //13
-        inputPoints.add(new Point(250, 200));   //14
-        inputPoints.add(new Point(250, 150));   //15
-        inputPoints.add(new Point(200, 150));   //16
-        inputPoints.add(new Point(200, 200));   //17
-        inputPoints.add(new Point(150, 200));   //18
-        inputPoints.add(new Point(150, 150));   //19
-        inputPoints.add(new Point(100, 150));   //20
+//        // 井字型 图形
+//        LinkedList<Point> inputPoints = new LinkedList<>();
+//        inputPoints.add(new Point(100, 100));      //1
+//        inputPoints.add(new Point(150, 100));    //2
+//        inputPoints.add(new Point(150, 50));   //3
+//        inputPoints.add(new Point(200, 50));     //4
+//        inputPoints.add(new Point(200, 100));   //5
+//        inputPoints.add(new Point(250, 100));   //6
+//        inputPoints.add(new Point(250, 50));    //7
+//        inputPoints.add(new Point(300, 50));    //8
+//        inputPoints.add(new Point(300, 100));   //9
+//        inputPoints.add(new Point(350, 100));   //10
+//        inputPoints.add(new Point(350, 150));   //11
+//        inputPoints.add(new Point(300, 150));   //12
+//        inputPoints.add(new Point(300, 200));   //13
+//        inputPoints.add(new Point(350, 200));   //14
+//        inputPoints.add(new Point(350, 250));   //15
+//        inputPoints.add(new Point(300, 250));   //16
+//        inputPoints.add(new Point(300, 300));   //17
+//        inputPoints.add(new Point(250, 300));   //18
+//        inputPoints.add(new Point(250, 250));   //19
+//        inputPoints.add(new Point(200, 250));   //20
+//        inputPoints.add(new Point(200, 300));   //21
+//        inputPoints.add(new Point(150, 300));   //22
+//        inputPoints.add(new Point(150, 250));   //23
+//        inputPoints.add(new Point(100, 250));   //24
+//        inputPoints.add(new Point(100, 200));   //25
+//        inputPoints.add(new Point(150, 200));   //26
+//        inputPoints.add(new Point(150, 150));   //26
+//        inputPoints.add(new Point(100, 150));   //26
+//        ArrayList<Rect> rects = SplitRect.Split2Rect(inputPoints);
+//        for (Rect r: rects)
+//            Log.i("# 图形", "Rect:"+r);
 
-        //  将第一个点加到队尾
-        inputPoints.add(new Point(100, 100));      //1
+//        // 普通 图形
+//        LinkedList<Point> inputPoints1 = new LinkedList<>();
+//        inputPoints1.add(new Point(100, 100));
+//        inputPoints1.add(new Point(150, 100));
+//        inputPoints1.add(new Point(150, 150));
+//        inputPoints1.add(new Point(100, 150));
+//        ArrayList<Rect> rects1 = SplitRect.Split2Rect(inputPoints1);
+//        for (Rect r: rects1)
+//            Log.i("普通 图形", "Rect:"+r);
+//        // |一 图形
+//        LinkedList<Point> inputPoints2 = new LinkedList<>();
+//        inputPoints2.add(new Point(100, 100));
+//        inputPoints2.add(new Point(200, 100));
+//        inputPoints2.add(new Point(200, 150));
+//        inputPoints2.add(new Point(150, 150));
+//        inputPoints2.add(new Point(150, 200));
+//        inputPoints2.add(new Point(100, 200));
+//        ArrayList<Rect> rects2 = SplitRect.Split2Rect(inputPoints2);
+//        for (Rect r: rects2)
+//            Log.i("|一 图形", "Rect:"+r);
+//        // _| 图形
+//        LinkedList<Point> inputPoints3 = new LinkedList<>();
+//        inputPoints3.add(new Point(100, 100));
+//        inputPoints3.add(new Point(150, 100));
+//        inputPoints3.add(new Point(150, 50));
+//        inputPoints3.add(new Point(200, 50));
+//        inputPoints3.add(new Point(200, 150));
+//        inputPoints3.add(new Point(100, 150));
+//        ArrayList<Rect> rects3 = SplitRect.Split2Rect(inputPoints3);
+//        for (Rect r: rects3)
+//            Log.i("_| 图形", "Rect:"+r);
+//        // |_ 图形
+//        LinkedList<Point> inputPoints4 = new LinkedList<>();
+//        inputPoints4.add(new Point(100, 100));
+//        inputPoints4.add(new Point(150, 100));
+//        inputPoints4.add(new Point(150, 150));
+//        inputPoints4.add(new Point(200, 150));
+//        inputPoints4.add(new Point(200, 200));
+//        inputPoints4.add(new Point(100, 200));
+//        ArrayList<Rect> rects4 = SplitRect.Split2Rect(inputPoints4);
+//        for (Rect r: rects4)
+//            Log.i("|_ 图形", "Rect:"+r);
+//        // 一|图形
+//        LinkedList<Point> inputPoints5 = new LinkedList<>();
+//        inputPoints5.add(new Point(100, 100));
+//        inputPoints5.add(new Point(200, 100));
+//        inputPoints5.add(new Point(200, 200));
+//        inputPoints5.add(new Point(150, 200));
+//        inputPoints5.add(new Point(150, 150));
+//        inputPoints5.add(new Point(100, 150));
+//        ArrayList<Rect> rects5 = SplitRect.Split2Rect(inputPoints5);
+//        for (Rect r: rects5)
+//            Log.i("一|图形", "Rect:"+r);
+//        // T 图形
+//        LinkedList<Point> inputPoints6 = new LinkedList<>();
+//        inputPoints6.add(new Point(100, 100));
+//        inputPoints6.add(new Point(250, 100));
+//        inputPoints6.add(new Point(250, 150));
+//        inputPoints6.add(new Point(200, 150));
+//        inputPoints6.add(new Point(200, 200));
+//        inputPoints6.add(new Point(150, 200));
+//        inputPoints6.add(new Point(150, 150));
+//        inputPoints6.add(new Point(100, 150));
+//        ArrayList<Rect> rects6 = SplitRect.Split2Rect(inputPoints6);
+//        for (Rect r: rects6)
+//            Log.i("T 图形", "Rect:"+r);
+//        //卜 字型
+//        LinkedList<Point> inputPoints7 = new LinkedList<>();
+//        inputPoints7.add(new Point(100, 100));
+//        inputPoints7.add(new Point(150, 100));
+//        inputPoints7.add(new Point(150, 150));
+//        inputPoints7.add(new Point(200, 150));
+//        inputPoints7.add(new Point(200, 200));
+//        inputPoints7.add(new Point(150, 200));
+//        inputPoints7.add(new Point(150, 250));
+//        inputPoints7.add(new Point(100, 250));
+//        ArrayList<Rect> rects7 = SplitRect.Split2Rect(inputPoints7);
+//        for (Rect r: rects7)
+//            Log.i("卜 字型", "Rect:"+r);
+//        //E 型
+//        LinkedList<Point> inputPoints8 = new LinkedList<>();
+//        inputPoints8.add(new Point(100, 100));
+//        inputPoints8.add(new Point(200, 100));
+//        inputPoints8.add(new Point(200, 150));
+//        inputPoints8.add(new Point(150, 150));
+//        inputPoints8.add(new Point(150, 200));
+//        inputPoints8.add(new Point(200, 200));
+//        inputPoints8.add(new Point(200, 250));
+//        inputPoints8.add(new Point(150, 250));
+//        inputPoints8.add(new Point(150, 300));
+//        inputPoints8.add(new Point(200, 300));
+//        inputPoints8.add(new Point(200, 350));
+//        inputPoints8.add(new Point(100, 350));
+//        ArrayList<Rect> rects8 = SplitRect.Split2Rect(inputPoints8);
+//        for (Rect r: rects8)
+//            Log.i("E 型", "Rect:"+r);
+//        // 十字型
+//        LinkedList<Point> inputPoints9 = new LinkedList<>();
+//        inputPoints9.add(new Point(100, 100));
+//        inputPoints9.add(new Point(150, 100));
+//        inputPoints9.add(new Point(150, 50));
+//        inputPoints9.add(new Point(200, 50));
+//        inputPoints9.add(new Point(200, 100));
+//        inputPoints9.add(new Point(250, 100));
+//        inputPoints9.add(new Point(250, 150));
+//        inputPoints9.add(new Point(200, 150));
+//        inputPoints9.add(new Point(200, 200));
+//        inputPoints9.add(new Point(150, 200));
+//        inputPoints9.add(new Point(150, 150));
+//        inputPoints9.add(new Point(100, 150));
+//        ArrayList<Rect> rects9 = SplitRect.Split2Rect(inputPoints9);
+//        for (Rect r: rects9)
+//            Log.i("十字型", "Rect:"+r);
 
-        ArrayList<Rect> rects = SplitRect.Split2Rect(inputPoints);
-        for (Rect r: rects)
-            System.out.println(r);
+        // test1字型
+        LinkedList<Point> inputPoints10 = new LinkedList<>();
+        inputPoints10.add(new Point(100, 100));      //1
+        inputPoints10.add(new Point(150, 100));    //2
+        inputPoints10.add(new Point(150, 50));   //3
+        inputPoints10.add(new Point(200, 50));     //4
+        inputPoints10.add(new Point(200, 100));   //5
+        inputPoints10.add(new Point(250, 100));   //6
+        inputPoints10.add(new Point(250, 50));    //7
+        inputPoints10.add(new Point(300, 50));    //8
+        inputPoints10.add(new Point(300, 100));   //9
+        inputPoints10.add(new Point(350, 100));   //10
+        inputPoints10.add(new Point(350, 150));   //11
+        inputPoints10.add(new Point(300, 150));   //12
+        inputPoints10.add(new Point(300, 200));   //13
+        inputPoints10.add(new Point(350, 200));   //14
+        inputPoints10.add(new Point(350, 250));   //15
+        inputPoints10.add(new Point(300, 250));   //16
+        inputPoints10.add(new Point(300, 300));   //17
+        inputPoints10.add(new Point(250, 300));   //18
+        inputPoints10.add(new Point(250, 250));   //19
+        inputPoints10.add(new Point(100, 250));   //20
+        ArrayList<Rect> rects10 = SplitRect.Split2Rect(inputPoints10);
+        for (Rect r: rects10)
+            Log.i("test1字型", "Rect:"+r);
     }
 }
--- app/src/main/java/com/cpd/yuqing/util/SplitRect.java
+++ app/src/main/java/com/cpd/yuqing/util/SplitRect.java
@@ -2,6 +2,7 @@ package com.cpd.yuqing.util;
 
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -15,25 +16,29 @@ public class SplitRect {
     public static ArrayList<Rect> Split2Rect(LinkedList<Point> points) {
         ArrayList<Rect> rects = new ArrayList<>();
         ArrayList<Point> pointsInRect = new ArrayList<>();  // 存放常规矩形的潜在顶点（预备顶点）
-        ArrayList<Integer> edgeInRect = new ArrayList<>();  // 存放常规矩形的边长
+        ArrayList<Edge> edgeInRect = new ArrayList<>();  // 存放常规矩形的边长
         while (!points.isEmpty()) {
             //  将点加入到临时列表中
             pointsInRect.add(points.poll());
             if (pointsInRect.size() > 1) {
                 // 当预备顶点中的顶点大于两个点后，可以计算边长
                 int lastPointIndexInRect = pointsInRect.size() - 1;
-                edgeInRect.add(getDistance(pointsInRect.get(lastPointIndexInRect - 1),
+                edgeInRect.add(getEdgeInfo(pointsInRect.get(lastPointIndexInRect - 1),
                         pointsInRect.get(lastPointIndexInRect)));
                 // 当边的数量大于等于3，就可以计算矩形的走势（内收型，外扩型）
                 int lastEdgeIndexInRect = edgeInRect.size() - 1;
                 if (edgeInRect.size() >= 3) {
                     // 获取要比较的两条平行的边
-                    int edge1 = edgeInRect.get(lastEdgeIndexInRect - 2);    //  要比较的第一条边
-                    int edge2 = edgeInRect.get(lastEdgeIndexInRect);        //  要比较的第二条边
+                    Edge edge1 = edgeInRect.get(lastEdgeIndexInRect - 2);    //  要比较的第一条边
+                    Edge edge2 = edgeInRect.get(lastEdgeIndexInRect);        //  要比较的第二条边
                     if (isInward(edge1, edge2)) {     //  内收型，可以分割出常规矩形，或直接获得常规矩形
                         Point first = pointsInRect.get(lastEdgeIndexInRect - 2);    //  第一条边的起点
                         Point end = pointsInRect.get(lastEdgeIndexInRect + 1);      //  第二条边的终点
-                        if (Math.abs(edge1) == Math.abs(edge2)) {     // 如果两条边的绝对值相等即认为找到一个常规的矩形
+                        if (Math.abs(edge1.distance) == Math.abs(edge2.distance)) {     // 如果两条边的绝对值相等即认为找到一个常规的矩形
+                            // 将end顶点放回队列的开头
+                            points.add(0, end);
+                            // 将first顶点放回到队列的尾部
+                            points.add(first);
                             // 根据定点确定矩形
                             Rect rect = getRect(pointsInRect);
                             rects.add(rect);
@@ -42,10 +47,10 @@ public class SplitRect {
                             edgeInRect.clear();
                         }
                         //  两条边的绝对值不想等,那么可以在较长的边截取一个新的点（点的平行向坐标值和较短的点的值一致），这个点和剩下的点可以组成一个常规的矩形
-                        if (Math.abs(edge1) < Math.abs(edge2)) {    //  第一条边较短
+                        if (Math.abs(edge1.distance) < Math.abs(edge2.distance)) {    //  第一条边较短
                             Point newPoint;
                             //  按照数据点的出现规律：横、纵、横、纵..... ，所以所在位置为偶数的边为横向边（边的两个定点的y值相同，x不同）
-                            if (lastEdgeIndexInRect % 2 == 0) {  // 横向边
+                            if (edge1.type == Type.HORIZONTAL) {  // 横向边
                                 newPoint = new Point(first.x, end.y);
                             } else {    // 纵向边
                                 newPoint = new Point(end.x, first.y);
@@ -54,29 +59,32 @@ public class SplitRect {
                             pointsInRect.remove(lastEdgeIndexInRect + 1);
                             //  添加新截取的点到 预备顶点
                             pointsInRect.add(newPoint);
-                            //  添加新截取的点到原始点链表队列中去
-                            points.add(newPoint);
+                            //  原始点链表队列 依次添加first newPoint end
+                            points.add(0, first);
+                            points.add(1, newPoint);
                             //  并将删去的点添加到原始点链表队列中去(这里两个点的顺序不能乱，也要按顺时针方向添加)
-                            points.add(end);
+                            points.add(2, end);
                             //  得到截取到的新矩形
                             Rect rect = getRect(pointsInRect);
                             rects.add(rect);
                             //  清除 pointsInRect、 edgeInRect中的数据，以便继续使用
                             pointsInRect.clear();
                             edgeInRect.clear();
-                        } else if (Math.abs(edge1) > Math.abs(edge2)) { // 第二条边较短
+                        } else if (Math.abs(edge1.distance) > Math.abs(edge2.distance)) { // 第二条边较短
                             Point newPoint;
-                            if (lastEdgeIndexInRect % 2 == 0) {  // 横向边
+                            if (edge1.type == Type.HORIZONTAL) {  // 横向边
                                 newPoint = new Point(end.x, first.y);
                             } else {    // 纵向边
                                 newPoint = new Point(first.x, end.y);
                             }
                             //  从 预备顶点 中删去无用的点
-                            pointsInRect.remove(lastEdgeIndexInRect + 1);
-                            // 添加新截取的点到 预备顶点
+                            pointsInRect.remove(lastEdgeIndexInRect - 2);
+                            //  添加新截取的点到 预备顶点
                             pointsInRect.add(newPoint);
-                            // 添加新截取的点到原始点链表队列中去 （注意：此时不用重新添加被删去的顶点，因为新截取的点和被删除的点以及后面的点，3点在一条边上）
-                            points.add(newPoint);
+                            //  将删去的点添加到原始点链表队列的队尾(这里两个点的顺序不能乱，也要按顺时针方向添加)      //todo 向原始点队列添加时应视end节点的下一个点所在位置决定是否添加、及添加的顺序
+                            points.add(first);
+                            //  添加新截取的点到原始点链表队列的队头
+                            points.add(0, newPoint);
                             //  得到截取到的新矩形
                             Rect rect = getRect(pointsInRect);
                             rects.add(rect);
@@ -119,11 +127,11 @@ public class SplitRect {
     }
 
     //  获得相邻两个点的距离
-    private static int getDistance(Point from, Point to) throws IllegalArgumentException {
+    private static Edge getEdgeInfo(Point from, Point to) throws IllegalArgumentException {
         if (from.x == to.x)
-            return to.y - from.y;
+            return new Edge(Type.VERTICAL, to.y - from.y);
         else if (from.y == to.y)
-            return to.x - from.x;
+            return new Edge(Type.HORIZONTAL, to.x - from.x);
         else
             throw new IllegalArgumentException("无法测出距离，不合理的点：from:" + from + "， to:" + to);
     }
@@ -131,7 +139,22 @@ public class SplitRect {
     //  通过两个整数的正负符号是否不同，来判断图形的走势。
     //  正负符号不同测证明是内收型图形，可以分割出一个规则矩形
     //  正负符号相同证明是外扩型图形，此时还不能够分割出一个规则矩形
-    private static boolean isInward(int i1, int i2) {
-        return (i1 > 0 && i2 < 0) || (i1 < 0 && i2 > 0);
+    private static boolean isInward(Edge e1, Edge e2) {
+        return (e1.distance > 0 && e2.distance < 0) || (e1.distance < 0 && e2.distance > 0);
+    }
+
+    // 边的走向：横、竖
+    enum Type {
+        HORIZONTAL, VERTICAL
+    }
+
+    private static class Edge {
+        Type type;
+        int distance;
+
+        Edge(Type type, int distance) {
+            this.type = type;
+            this.distance = distance;
+        }
     }
 }
