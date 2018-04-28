7d171a225493f0914a67a7739dfb478a9007f472
s21v <s21vwy@aliyun.com>
2018-04-28 17:56:03 +0800

    添加数字报部分

--- app/src/main/java/com/cpd/yuqing/view/VerticalOverLayout.kt
+++ app/src/main/java/com/cpd/yuqing/view/VerticalOverLayout.kt
@@ -2,28 +2,44 @@ package com.cpd.yuqing.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import java.security.MessageDigest
 
 /**
  * Created by s21v on 2018/4/27.
  */
 class VerticalOverLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+    // child view高度
+    val heightSize: Int
+    init {
+        // 获得屏幕高度
+        val dm = DisplayMetrics()
+        val wm: WindowManager = context!!.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        wm.defaultDisplay.getMetrics(dm)
+        val screenHeight = dm.heightPixels
+        // 获得手机状态栏高度
+        val resourceID = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        var statusBarHeight = 0
+        if (resourceID > 0)
+            statusBarHeight = context.resources.getDimensionPixelSize(resourceID)
+        heightSize = screenHeight - statusBarHeight
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        Log.i("VerticalOverLayout", "heightMeasureSpec size:${MeasureSpec.getSize(heightMeasureSpec)}")
         var totalHeight = 0
         for (i in 0 until childCount) {
             val child = getChildAt(i)
-            child.measure(widthMeasureSpec, heightMeasureSpec)
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY))
             totalHeight += child.measuredHeight
-            Log.i("VerticalOverLayout", "child $i, height:${child.measuredHeight}")
         }
         val finalHeightMeasureSpec = MeasureSpec.makeMeasureSpec(totalHeight, MeasureSpec.EXACTLY)
         setMeasuredDimension(getDefaultSize(suggestedMinimumWidth, widthMeasureSpec),
                 getDefaultSize(suggestedMinimumHeight, finalHeightMeasureSpec))
-        Log.i("VerticalOverLayout", "measuredHeight:$measuredHeight")
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
